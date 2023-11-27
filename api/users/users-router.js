9f32f88d6fbceb2b87305429814aaa8225eff277
naushinhoque <fnmh@yahoo.com>
2023-11-27 16:29:46 -0600

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-  validateUserId
+  validateUserId,
+  validateUser
 } = require('../middleware/middleware')
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -16,12 +17,12 @@ router.get('/:id', validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
